3211389733c1a7eade3431960c97e499a137ac18
SmutnaZabaJpg <99398267+smutnazabajpg@users.noreply.github.com>
2024-04-19 13:03:29 +0200

    Add files via upload

---
+++ 19.04.2024/uzytkownicy.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Portal Społecznościowy</title>
+        <link rel="stylesheet" href="styl5.css">
+    </head>
+    <body>
+        <div id="bannerLeft">
+            <h2>Nasze osiedle</h2>
+        </div>
+        <div id="bannerRight">
+            
+        </div>
+        <div id="blockLeft">
+            <h3>Logowanie</h3>
+            <form method="POST">
+                login</br>
+                <input type="text" name="login"></br>
+                hasło</br>
+                <input type="password" name="password"></br>
+                <input type="submit" name="submit" value="Zaloguj">
+            </form>
+        </div>
+        <div id="blockRight">
+            <h3>Wizytówka</h3>
+            <div id="card"></div>
+        </div>
+        <div id="footer">Stronę wykonał: Wściekły Świstak</div>
+    </body>
+</html>
